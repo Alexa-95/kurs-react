d1d9bbf84bdd70cae053ba1b6bdecdb900b28ca3
Aleksandra Sobaska <Aleksandra.Makowska@grupawp.pl>
2020-04-03 21:53:37 +0200

    init

---
+++ Header.js
@@ -0,0 +1,16 @@
+const Header = props => {
+  const activeItems = props.items.filter(item => item.active);
+  const number = activeItems.length;
+  return (
+    <header>
+      <h2>Wielkość zamówienia: {number}</h2>
+      <h2>
+        {" "}
+        Do zapłaty:{" "}
+        {number
+          ? `${number * 10} złotych`
+          : "Nie kupujesz to nie płacissz"}{" "}
+      </h2>
+    </header>
+  );
+};
